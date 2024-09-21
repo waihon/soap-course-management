0442ca00680bcbf59d55e50680a625a3e6a9b6e7
Waihon Yew <yewwaihon@gmail.com>
2024-09-21 15:15:29 +0800

    Initial version generated by Spring Initializr

---
+++ src/test/java/com/waihon/soap/webservices/soapcoursemanagement/SoapCourseManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waihon.soap.webservices.soapcoursemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapCourseManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
